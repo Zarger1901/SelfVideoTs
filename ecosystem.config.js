4cc085e059b500ee6627b2a61d3346cb7c979044
zarger <carvel1901@icloud.com>
2023-01-14 01:45:27 +0800

    update repository by zarger 2023.01.14

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: 'web-app',
+            name: 'VideoServer',
             script: './build/server.js',
             instances: 'max',
             exec_mode: 'cluster',
